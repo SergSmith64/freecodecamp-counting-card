5c8790bede11b37aff77d816fec1818f8383fe2d
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-28 09:57:31 +0400

    feat: add switch

---
+++ index.js
@@ -0,0 +1,38 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+  }
+  return count;
+  // Only change code above this line
+}
+cc(2); cc(3); cc(4); cc(5); cc(6);
+
+// running tests
+// Cards Sequence 2, 3, 4, 5, 6 should return 5 Bet
+// Cards Sequence 7, 8, 9 should return 0 Hold
+// Cards Sequence 10, J, Q, K, A should return -5 Hold
+// Cards Sequence 3, 7, Q, 8, A should return -1 Hold
+// Cards Sequence 2, J, 9, 2, 7 should return 1 Bet
+// Cards Sequence 2, 2, 10 should return 1 Bet
+// Cards Sequence 3, 2, A, 10, K should return -1 Hold
+// tests completed
