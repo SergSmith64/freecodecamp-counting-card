0bfc3fcf6f4a6cb45ffecfe5201af7474fd5deb9
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-30 14:27:02 +0400

    feat: bracket notation

---
+++ bracket_notation.js
@@ -0,0 +1,13 @@
+var dogs = {
+    Fido: "Mutt",
+    Hunter: "Doberman",
+    Snoopie: "Beagle",
+    Kid: "Tyke"
+}
+console.log("__dogs ___", dogs);
+
+var myDog = "Hunter";
+console.log("__my Dog ___", myDog);
+
+var myBreed = dogs[myDog];
+console.log("__myBreed ___", myBreed); // Doberman
