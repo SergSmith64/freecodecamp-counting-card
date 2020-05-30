985dc5bc110a638318a26e941112056d5615d2e4
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-31 01:37:37 +0400

    fix

--- used-method-hasOwnProperty.js
+++ used-method-hasOwnProperty.js
@@ -1,17 +1,20 @@
 function checkObj(obj, checkProp) {
-    // Only change code below this line
+
     console.log("__OBJ__", obj, "______key: ", checkProp);
     if (obj.hasOwnProperty(checkProp)) {
         console.log("__obj.checkProp__", obj.checkProp);
-        return obj.checkProp;
+        return obj["checkProp"];
     } else {
         console.log("__Not Found__");
         return "Not Found";
     }
-    // Only change code above this line
-  }
 
+  }
   var obj1 = {gift: "pony", pet: "kitten", bed: "sleigh"}
-  var result1 = checkObj(obj1, "gift");
+  var result1 = checkObj(obj1, "gggg");
   console.log("_result1__ ", result1);
 
+// __ WORK __
+// var newObj = {top: "hat", bottom: "pants"}
+// console.log(newObj.hasOwnProperty("top"));
+// console.log(newObj.hasOwnProperty("middle"));
