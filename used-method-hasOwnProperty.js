5a5c403d1ea0dd1f1713046edad6a4d248bc6ce6
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-31 11:08:42 +0400

    feat: add MDN - bypass properties of an object

--- used-method-hasOwnProperty.js
+++ used-method-hasOwnProperty.js
@@ -1,20 +1,42 @@
-function checkObj(obj, checkProp) {
+console.log("Hello");
 
-    console.log("__OBJ__", obj, "______key: ", checkProp);
-    if (obj.hasOwnProperty(checkProp)) {
-        console.log("__obj.checkProp__", obj.checkProp);
-        return obj["checkProp"];
-    } else {
-        console.log("__Not Found__");
-        return "Not Found";
-    }
+// function checkObj(obj, checkProp) {
 
-  }
-  var obj1 = {gift: "pony", pet: "kitten", bed: "sleigh"}
-  var result1 = checkObj(obj1, "gggg");
-  console.log("_result1__ ", result1);
+//     console.log("__OBJ__", obj, "______key: ", checkProp);
+//     if (obj.hasOwnProperty(checkProp)) {
+//         console.log("__obj.checkProp__", obj.checkProp);
+//         return obj["checkProp"];
+//     } else {
+//         console.log("__Not Found__");
+//         return "Not Found";
+//     }
+//   }
+//   var obj1 = {gift: "pony", pet: "kitten", bed: "sleigh"};
+//   console.log("_значение поля GIFT__ - ",obj1.gift);
+//   var result1 = checkObj(obj1, "gift");
+//   console.log("_result1__ ", result1);
 
 // __ WORK __
 // var newObj = {top: "hat", bottom: "pants"}
 // console.log(newObj.hasOwnProperty("top"));
 // console.log(newObj.hasOwnProperty("middle"));
+
+
+// MDN: обход свойств объекта - bypass properties of an object
+var buz = {
+	fog: 'stack',
+	rain: 'very',
+	sun: 'nothing',
+	mass: ['11','22','33']
+};
+console.log("_Объект__", buz);
+  
+  for (var name in buz) {
+	console.log("_name__ =", name);
+	if (buz.hasOwnProperty(name)) {
+		console.log('это точно туман (' + name + '). Значение: ' + buz[name]);
+	}
+	else {
+		console.log(name); // toString или что-то ещё
+	}
+  }
