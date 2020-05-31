063334055710a9b393da47db462117a8b22028b7
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-31 15:04:22 +0400

    new task: record-collection

---
+++ t092-record-collection.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+
+var collection = {
+    2548: {
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: [
+        "You Give Love a Bad Name"
+      ]
+    },
+    2468: {
+      album: "1999",
+      artist: "Prince",
+      tracks: [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    1245: {
+      artist: "Robert Palmer",
+      tracks: [ ]
+    },
+    5439: {
+      album: "ABBA Gold"
+    }
+  };
+
+console.log("__ДО РЕДАКТИРОВАНИЯ__", collection);
+console.log("End");
