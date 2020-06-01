84dcdf269143ab1441c581abfbded4681b71e323
SergSmith64 <sergsmith2008@yandex.ru>
2020-06-01 16:31:10 +0400

    fix

--- t092-record-collection.js
+++ t092-record-collection.js
@@ -1,29 +1,54 @@
 console.log("Hello");
 
 var collection = {
-    2548: {
-      album: "Slippery When Wet",
-      artist: "Bon Jovi",
-      tracks: [
-        "You Give Love a Bad Name"
-      ]
-    },
-    2468: {
-      album: "1999",
-      artist: "Prince",
-      tracks: [
-        "1999",
-        "Little Red Corvette"
-      ]
-    },
-    1245: {
-      artist: "Robert Palmer",
-      tracks: [ ]
-    },
-    5439: {
-      album: "ABBA Gold"
-    }
-  };
+	2548: {
+		album: "Slippery When Wet",
+		artist: "Bon Jovi",
+		tracks: [
+			"You Give Love a Bad Name"
+		]
+	},
+	2468: {
+		album: "1999",
+		artist: "Prince",
+		tracks: [
+			"1999",
+			"Little Red Corvette"
+		]
+	},
+	1245: {
+		artist: "Robert Palmer",
+		tracks: []
+	},
+	5439: {
+		album: "ABBA Gold"
+	}
+};
 
 console.log("__ДО РЕДАКТИРОВАНИЯ__", collection);
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+	console.log("__prop______=", prop);
+	if (value === "") delete collection[id][prop];
+	else if (prop === "tracks") {
+		collection[id][prop] = collection[id][prop] || [];
+		collection[id][prop].push(value);
+	} else {
+		collection[id][prop] = value;
+	}
+	console.log("__Value_____=", value);
+	console.log("__tracks__=", "tracks");
+	return collection;
+}
+
+// __ UPDATE ___
+updateRecords(5439, "artist", "ABBA");
+
+console.log("__ ПОСЛЕ 1-го АПДЕЙТА __", collection);
+
+updateRecords(5439, "tracks", "Vaterloo", "Happy Year");
+
+console.log("__ ПОСЛЕ 2-го АПДЕЙТА __", collection);
+
 console.log("End");
