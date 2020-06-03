b831aa21ac594d856eea1c410f60a49d7a2bb29d
SergSmith64 <sergsmith2008@yandex.ru>
2020-06-04 02:05:07 +0400

    feat: do-while-loop

---
+++ t099-do-while-loops.js
@@ -0,0 +1,41 @@
+console.log("Hello");
+
+// _ цикл сначала выполняет один проход кода, затем проверяет условие _
+// var ourArray = [];
+// var i = 0;
+// do {
+//   ourArray.push(i);
+//   console.log("_массив_", ourArray);
+//   i++;
+// } while (i < 5);
+
+
+// _ условие не выполняется при первой проверке _
+// var ourArray = []; 
+// var i = 5;
+// while (i < 5) {
+//   ourArray.push(i);
+//   console.log("_массив_", ourArray);
+//   i++;
+// }
+// console.log("_массив_", ourArray);
+
+
+// задание
+
+var myArray = [];
+console.log("_массив_", myArray);
+var i = 10;
+// Only change code below this line
+do {
+  myArray.push(i);
+  console.log("_массив_ =", myArray);
+  i++;
+}
+while (i < 11)
+
+
+
+
+console.log("End");
+
