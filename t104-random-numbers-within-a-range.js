72873915a6a957d3b9341ede18d5f84c7bef4c34
SergSmith64 <sergsmith2008@yandex.ru>
2020-06-08 00:34:31 +0400

    feat: Random Whole Numbers within a Range

---
+++ t104-random-numbers-within-a-range.js
@@ -0,0 +1,11 @@
+// Random Whole Numbers within a Range
+// Случайные целые числа в заданном диапазоне
+
+console.log("Hello");
+
+var min = 120;
+var max = 123;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log("End");
